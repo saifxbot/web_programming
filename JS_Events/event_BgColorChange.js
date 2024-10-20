eb2404567dfb049bdc6bd88badc9c64a3321e687
Saif Ibna Reza <saifibnareza078@gmail.com>
2024-10-20 13:22:14 +0300

    adding BgColor to events

---
+++ JS_Events/event_BgColorChange.js
@@ -0,0 +1,8 @@
+function changeColor(event) {
+    const selectedColor = event.target.value;
+    document.body.style.backgroundColor = selectedColor;
+}
+document.addEventListener('DOMContentLoaded', () =>{
+    const colorPicker = document.getElementById('colorPicker');
+    colorPicker.addEventListener('input', changeColor);
+});
--- JS_Events/event_hide_paragraph.js
+++ JS_Events/event_hide_paragraph.js
@@ -1,15 +1,13 @@
-// Function to hide the paragraph by setting visibility to 'hidden'
 function hideParagraph() {
     document.getElementById('hide').style.visibility = 'hidden';
 }
 
-// Function to show the paragraph by setting visibility to 'visible'
 function showParagraph() {
     document.getElementById('hide').style.visibility = 'visible';
 }
 
-// Adding event listeners via JavaScript
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('seek').addEventListener('mouseover', showParagraph);
     document.getElementById('hide').addEventListener('mouseover', hideParagraph);
 });
+
