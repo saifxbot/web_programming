b347d4be0a03d195aa1e9b9811e3597a6c643cce
Saif Ibna Reza <saifibnareza078@gmail.com>
2024-10-27 09:56:35 +0300

    adding random number guessing files

---
+++ else_ifStatement/random_number.js
@@ -0,0 +1,17 @@
+function checkUserNumber(userGuess) {
+    let randomNumber = Math.floor(Math.random()*10) + 1;
+    if (userGuess===randomNumber) {
+        return('Yes, you quessed right');
+    } else if(userGuess>randomNumber) {
+        return('Your guess was too high.');
+    } else {
+        return('Your guess eas too low');
+    }
+}
+function guessNumber() {
+    let userGuess= parseInt(document.getElementById('guessInput').value);
+    let result= checkUserNumber(userGuess);
+    document.getElementById('result').textContent= result;
+}
+console.log(checkUserNumber(5));
+console.log(checkUserNumber(6));
